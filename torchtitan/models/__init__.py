5139f4350ff819359b04ae394fa8bafe7181909d
Xingyao Wang <xingyao6@illinois.edu>
2024-07-16 22:13:13 -0500

    initialize solo/duet from llama3

--- torchtitan/models/__init__.py
+++ torchtitan/models/__init__.py
@@ -5,15 +5,22 @@
 # LICENSE file in the root directory of this source tree.
 
 from torchtitan.models.llama import llama2_configs, llama3_configs, Transformer
+from torchtitan.models.duet import duet_configs, Transformer as DuetTransformer
 
 models_config = {
     "llama2": llama2_configs,
     "llama3": llama3_configs,
+    "duet": duet_configs,
 }
 
-model_name_to_cls = {"llama2": Transformer, "llama3": Transformer}
+model_name_to_cls = {
+    "llama2": Transformer,
+    "llama3": Transformer,
+    "duet": DuetTransformer,
+}
 
 model_name_to_tokenizer = {
     "llama2": "sentencepiece",
     "llama3": "tiktoken",
+    "duet": "sentencepiece",
 }
