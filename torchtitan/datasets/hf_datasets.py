29aab3ea5769a6e5bae52f32e4c1ead9ac213969
Yangyi Chen <yangyichen6666@gmail.com>
2024-09-14 04:01:03 -0500

    add imagenet text pretraining

--- torchtitan/datasets/hf_datasets.py
+++ torchtitan/datasets/hf_datasets.py
@@ -24,16 +24,151 @@ from torchtitan.logging_utils import logger
 
 from datasets import load_dataset
 from datasets.distributed import split_dataset_by_node
-
+from torchtitan.datasets.image_utils import convert_image_base64_to_patches
 # map from dataset name to a local directory, or
 # a dataset repository on the HF hub
 _supported_datasets = {
     "c4_mini": "torchtitan/datasets/c4_mini",
     "c4": "allenai/c4",
+    'imagenet': "/shared/nas/data/m1/yangyic3/Multimodal-Mistral/data/processed/imagenet/"
 }
 
 
-class HuggingFaceDataset(IterableDataset, Stateful):
+# class HuggingFaceDataset(IterableDataset, Stateful):
+#     """PyTorch Representation of the HuggingFace Dataset.
+
+#     Args:
+#         dataset_name (str): name of the dataset to load
+#         dataset_path (Optional[str]):
+#             Path to the dataset in the file system. If provided, data will be loaded
+#             from this path instead of downloaded.
+#         tokenizer (Tokenizer):
+#             Tokenizer used to encode data. Tokenize must implement an `encode` and `decode` method.
+#         seq_len (int): max sequence length
+#         world_size (int): number of data parallel processes participating in training
+#         rank (int): rank of the current data parallel process
+#         infinite (bool): whether to loop infinitely over the dataset
+
+#     We currently support the c4 dataset and a subset of it:
+#     c4_mini (45K training entries)
+#     c4 (177M training entries - this dataset is streamed due to the size)
+
+#     >> c4 (EN) <<:
+#     c4 cleaned, English version
+#     Data input format (c4):
+#     {
+#     'url': 'https://klyq.com/beginners-bbq-class-taking-place-in-missoula/',
+#     'text': 'Beginners BBQ Class Taking Place in Missoula!\nDo you want to get better at ...',
+#     'timestamp': '2019-04-25T12:57:54Z'
+#     }
+
+#     Example use (c4):
+#     >>> ds = HuggingFaceDataset(dataset_name="c4", dataset_path=None, tokenizer=tokenizer)
+#     >>> for batch in Dataloader(ds, batch_size=8):
+#             print(f"Batch size: {len(batch)}")
+#         Batch size: 8
+#     """
+
+#     def __init__(
+#         self,
+#         dataset_name: str,
+#         dataset_path: Optional[str],
+#         tokenizer: Tokenizer,
+#         seq_len: int = 2048,
+#         world_size: int = 1,
+#         rank: int = 0,
+#         infinite: bool = False,
+#     ) -> None:
+#         # allow user to pass in a (local or HF hub) path to use unsupported datasets
+#         if dataset_name not in _supported_datasets:
+#             if dataset_path:
+#                 logger.warning(
+#                     f"Dataset {dataset_name} is not tested or verfied. "
+#                     f"Recommended datasets are: {list(_supported_datasets.keys())}."
+#                 )
+#             else:
+#                 raise ValueError(
+#                     f"Dataset {dataset_name} is not supported. "
+#                     f"Supported datasets are: {list(_supported_datasets.keys())}."
+#                 )
+
+#         if not dataset_path:
+#             dataset_path = _supported_datasets[dataset_name]
+#         logger.info(f"Preparing {dataset_name} dataset from {dataset_path}")
+
+#         if dataset_name == "c4":
+#             # c4 is huge, and requires both streaming and language selection
+#             # (we default to en)
+#             ds = load_dataset(dataset_path, name="en", split="train", streaming=True)
+#         else:
+#             ds = load_dataset(dataset_path, split="train")
+
+#         # TODO: support shuffling and checkpointing
+#         self.dataset_name = dataset_name
+#         self._data = split_dataset_by_node(ds, rank, world_size)
+#         self._tokenizer = tokenizer
+#         self.seq_len = seq_len
+#         self.infinite = infinite
+
+#         # variables for checkpointing
+#         self._sample_idx = 0
+#         self._all_tokens: List[int] = []
+        
+
+#     def __iter__(self):
+#         max_buffer_token_len = 1 + self.seq_len
+
+#         while True:
+#             for sample in self._get_data_iter():
+#                 sample_text = sample["text"]
+#                 sample_tokens = self._tokenizer.encode(sample_text, bos=True, eos=True)
+#                 self._all_tokens.extend(sample_tokens)
+#                 self._sample_idx += 1
+
+#                 while len(self._all_tokens) >= max_buffer_token_len:
+#                     x = torch.LongTensor(self._all_tokens[:max_buffer_token_len])
+#                     # update tokens to the remaining tokens
+#                     self._all_tokens = self._all_tokens[max_buffer_token_len:]
+#                     input = x[:-1]
+#                     label = x[1:]
+#                     yield input, label
+
+#             if not self.infinite:
+#                 logger.warning(f"Dataset {self.dataset_name} has run out of data.")
+#                 break
+#             else:
+#                 # Reset offset for the next iteration
+#                 self._sample_idx = 0
+#                 logger.warning(
+#                     f"Dataset {self.dataset_name} is being re-looped. "
+#                     "Loss related metrics might be misleading."
+#                 )
+
+#     def _get_data_iter(self):
+#         if self._sample_idx == 0:
+#             return iter(self._data)
+
+#         # Skip samples
+#         if isinstance(self._data, IterableDataset):
+#             it = iter(self._data)
+#             # Naively iterate through the samples as skip may not be supported
+#             for _ in range(self._sample_idx):
+#                 next(it)
+#             return it
+
+#         # As skipping to the end throws an error in case of map-style dataset, return an empty iterator
+#         if self._sample_idx == len(self._data):
+#             return iter([])
+#         return iter(self._data.skip(self._sample_idx))
+
+#     def load_state_dict(self, state_dict):
+#         self._sample_idx = state_dict["sample_idx"]
+#         self._all_tokens = state_dict["token_buffer"]
+
+#     def state_dict(self):
+#         return {"token_buffer": self._all_tokens, "sample_idx": self._sample_idx}
+
+class HuggingFaceDatasetVL(IterableDataset, Stateful):
     """PyTorch Representation of the HuggingFace Dataset.
 
     Args:
@@ -99,6 +234,8 @@ class HuggingFaceDataset(IterableDataset, Stateful):
             # c4 is huge, and requires both streaming and language selection
             # (we default to en)
             ds = load_dataset(dataset_path, name="en", split="train", streaming=True)
+        elif dataset_name == 'imagenet':
+            ds = load_dataset(dataset_path, split="train", streaming=True)
         else:
             ds = load_dataset(dataset_path, split="train")
 
@@ -112,13 +249,27 @@ class HuggingFaceDataset(IterableDataset, Stateful):
         # variables for checkpointing
         self._sample_idx = 0
         self._all_tokens: List[int] = []
+        
 
     def __iter__(self):
         max_buffer_token_len = 1 + self.seq_len
 
         while True:
             for sample in self._get_data_iter():
-                sample_text = sample["text"]
+                content = sample['content']
+                content_bef = content[0]
+                content_aft = content[1]
+                if content_bef['type'] == 'text':
+                    sample_text = content_bef['text']
+                    image = content_aft['image_url']['url']
+                else:
+                    sample_text = content_aft['text']
+                    image = content_bef['image_url']['url']
+                patches = convert_image_base64_to_patches(image)
+                
+                
+                
+                
                 sample_tokens = self._tokenizer.encode(sample_text, bos=True, eos=True)
                 self._all_tokens.extend(sample_tokens)
                 self._sample_idx += 1
@@ -194,6 +345,21 @@ class DPAwareDataLoader(StatefulDataLoader, Stateful):
         super().load_state_dict(pickle.loads(state_dict[self._rank_id]))
 
 
+# def build_hf_data_loader(
+#     dataset_name: str,
+#     dataset_path: Optional[str],
+#     tokenizer: Tokenizer,
+#     batch_size: int,
+#     seq_len: int,
+#     world_size,
+#     rank,
+#     infinite: bool = True,
+# ):
+#     hf_ds = HuggingFaceDataset(
+#         dataset_name, dataset_path, tokenizer, seq_len, world_size, rank, infinite
+#     )
+
+#     return DPAwareDataLoader(rank, hf_ds, batch_size=batch_size)
 def build_hf_data_loader(
     dataset_name: str,
     dataset_path: Optional[str],
@@ -204,8 +370,9 @@ def build_hf_data_loader(
     rank,
     infinite: bool = True,
 ):
-    hf_ds = HuggingFaceDataset(
+    hf_ds = HuggingFaceDatasetVL(
         dataset_name, dataset_path, tokenizer, seq_len, world_size, rank, infinite
     )
 
     return DPAwareDataLoader(rank, hf_ds, batch_size=batch_size)
+
